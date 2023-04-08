4343241fcf2bf87b52b7f2cc1427df98863ed8eb
Richard Livolsi <richard.livolsi@goziohealth.com>
2023-04-08 15:48:01 -0400

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+/* Copyright (c) 2023 Coderich LLC. All Rights Reserved. */
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        node: 'current',
+      },
+    },
+    ],
+  ],
+};
