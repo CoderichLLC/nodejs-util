0aeb2fd2e9428175784d9ce8ec59fd24bbfc4767
Richard Livolsi <richard.livolsi@goziohealth.com>
2024-11-08 15:34:42 -0500

    Updated deps; wrote perf test

---
+++ test/perf.test.js
@@ -0,0 +1,41 @@
+const Crypto = require('crypto');
+const Flat = require('flat');
+const { flatten, unflatten } = require('../src');
+
+describe('performance', () => {
+  let largeObj;
+
+  beforeAll(() => {
+    let depth = 0;
+    const iterator = Array.from(new Array(100));
+    const reducer = obj => iterator.reduce((prev) => {
+      return Object.assign(prev, iterator.reduce(p => Object.assign(p, {
+        [Crypto.randomBytes(16).toString('hex')]: ++depth > 10 ? Crypto.randomBytes(16).toString('hex') : reducer({}),
+      }), {}));
+    }, obj);
+    largeObj = reducer({});
+  });
+
+  test('flatten/unflatten', () => {
+    console.time('Flat.flatten');
+    const flat = Flat.flatten(largeObj);
+    console.timeEnd('Flat.flatten');
+
+    console.time('Flat.unflatten');
+    const unflat = Flat.unflatten(flat);
+    console.timeEnd('Flat.unflatten');
+
+    console.time('flatten');
+    const fl = flatten(largeObj);
+    console.timeEnd('flatten');
+
+    console.time('unflatten');
+    const unfl = unflatten(fl);
+    console.timeEnd('unflatten');
+
+    expect(fl).toEqual(flat);
+    expect(unfl).toEqual(unflat);
+    expect(unflat).toEqual(largeObj);
+    expect(unfl).toEqual(largeObj);
+  });
+});
