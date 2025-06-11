d8fc38981032edb8eba86c3633aa5513211f7772
Richard Livolsi <richard.livolsi@goziohealth.com>
2025-06-11 14:03:39 -0400

    Engine 22

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 2.0.0
+- Node22 engine
+
 ## 1.2.1
 - Added flatten option.ignorePaths
 - Added traverse method
--- src/index.js
+++ src/index.js
@@ -212,7 +212,7 @@ exports.pipeline = (thunks, startValue) => {
 
 exports.requireDir = (dir) => {
   return exports.dirmap(dir, ({ path }) => {
-    return path.includes('.js') ? require(path) : path; // eslint-disable-line import/no-dynamic-require, global-require
+    return path.includes('.js') ? require(path) : path;
   });
 };
 
